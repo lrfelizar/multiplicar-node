5d58eb5d824e00bc6257adef132b1572a3678dad
Laura <lrfelizar@gmail.com>
2020-06-06 15:01:28 -0600

    Modificacion final

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
 let comando = argv._[0];
 
+
 switch (comando) {
+
     case 'listar':
         listarTabla(argv.base, argv.limite);
         break;
+
     case 'crear':
         crearArchivo(argv.base, argv.limite)
             .then(archivo => console.log(`Archivo creado: `, colors.green(archivo)))
@@ -17,10 +20,11 @@ switch (comando) {
 
     default:
         console.log('Comando no reconocido');
+
 }
 
 
-// console.log('Limite', argv.limite);
 
+// console.log('Limite', argv.limite);
 // let parametro = argv[2];
 // let base = parametro.split('=')[1];
